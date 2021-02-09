650b0571d85fcd879f9fa11d3cb14d404c09e23a
Papaya Delight <jasmineteters2020@gmail.com>
2021-02-08 22:19:28 -0700

    fixed the filter and search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import './index.css';
-import { HiOutlineFilter } from 'react-icons/hi';
+import { HiArrowNarrowUp } from 'react-icons/hi';
+import { HiArrowNarrowDown } from 'react-icons/hi';
 
 function App() {
   const [userArray, setUserArray] = useState([]);
 
   const [filteredArray, setFilteredArray] = useState([]);
+  
+  const [searchArray, setSearchArray] = useState([]);
 
   useEffect(() => {
     axios
@@ -22,12 +25,28 @@ function App() {
       .catch((err) => console.log(err));
   }, []);
 
-function handleNameClickF(event) {
-  event.preventDefault();
-  const sortedArray = setUserArray.sort((a, b) =>
-  a.name.first.localeCompare(b.name.first),
-  setFilteredArray(sortedArray)
+
+function sortAsc(key){
+  let ascArray = [...userArray].sort((a, b) =>
+    a.name[key] > b.name[key] ? 1 : -1,
   );
+  setFilteredArray(ascArray)
+}
+
+function sortDec(key) {
+  let decArray = [...userArray].sort((a, b) =>
+    b.name[key] > a.name[key] ? 1 : -1,
+  );
+  setFilteredArray(decArray);
+  console.log('help')
+}
+
+function search(e){
+  setSearchArray(e.target.value)
+  var newArray = [...userArray].filter(item => {
+    return item.name.first.toLowerCase().includes(e.target.value.toLowerCase());
+  })
+  setFilteredArray(newArray);
 }
 
 
@@ -38,17 +57,40 @@ function handleNameClickF(event) {
           Employee Tracker
         </h1>
       </header>
+      <input
+        class='bg-white p-4 text-black'
+        value={searchArray}
+        onChange={search}
+      />
       <div className='align-middle inline-block min-w-full shadow overflow-hidden bg-white shadow-dashboard px-8 pt-3 rounded-bl-lg rounded-br-lg'>
         <table className='min-w-full '>
           <thead className=''>
             <tr>
               <th className='tracking-wider px-6 py-3 ' scope='col'>
                 First Name
-                <HiOutlineFilter type='button' className='ml-2 my-auto float-right' onClick={handleNameClickF}/>
+                <HiArrowNarrowUp
+                  type='button'
+                  className='ml-2 my-auto float-right'
+                  onClick={() => sortAsc('first')}
+                />
+                <HiArrowNarrowDown
+                  type='button'
+                  className='ml-2 my-auto float-right'
+                  onClick={() => sortDec('first')}
+                />
               </th>
               <th className='tracking-wider px-6 py-3 ' scope='col'>
                 Last Name
-                <HiOutlineFilter className='ml-2 my-auto float-right' />
+                <HiArrowNarrowUp
+                  type='button'
+                  className='ml-2 my-auto float-right'
+                  onClick={() => sortAsc('last')}
+                />
+                <HiArrowNarrowDown
+                  type='button'
+                  className='ml-2 my-auto float-right'
+                  onClick={() => sortDec('last')}
+                />
               </th>
 
               <th className='tracking-wider px-6 py-3 ' scope='col'>
@@ -62,7 +104,7 @@ function handleNameClickF(event) {
                 scope='col'
               >
                 Age
-                <HiOutlineFilter className='ml-2 my-auto float-right' />
+                <HiArrowNarrowUp className='ml-2 my-auto float-right' />
               </th>
             </tr>
           </thead>
